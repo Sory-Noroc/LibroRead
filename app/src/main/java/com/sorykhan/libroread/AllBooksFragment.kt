a5f9d51d17b2f0678d80249cd82c48f931ec6d14
Sory <sorinnoroc1@gmail.com>
2022-08-18 23:24:48 +0300

    Implemented the fragments of MainActivity

--- app/src/main/java/com/sorykhan/libroread/AllBooksFragment.kt
+++ app/src/main/java/com/sorykhan/libroread/AllBooksFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.sorykhan.libroread.adapter.BookItemAdapter
+import com.sorykhan.libroread.database.Book
 import com.sorykhan.libroread.databinding.FragmentAllBooksBinding
+import com.sorykhan.libroread.viewmodels.BookListViewModel
 
 class AllBooksFragment : Fragment() {
 
@@ -15,19 +20,26 @@ class AllBooksFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var datasource: List<Book>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-//        val allBooksViewModel =
-//            ViewModelProvider(this).get(AllBooksViewModel::class.java)
+        val sharedViewModel = ViewModelProvider(this).get(BookListViewModel::class.java)
+        datasource = sharedViewModel.getAllBooks().value ?: emptyList()
 
         _binding = FragmentAllBooksBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.allBooksRecyclerView
+        recyclerView.adapter = BookItemAdapter(requireContext(), datasource)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/sorykhan/libroread/MainActivity.kt
+++ app/src/main/java/com/sorykhan/libroread/MainActivity.kt
@@ -38,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_all_books, R.id.nav_favorites, R.id.nav_started_books
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
