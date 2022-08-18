07727f44a5c2690f438d804d3873a0804083bbef
Sory <sorinnoroc1@gmail.com>
2022-08-18 23:34:59 +0300

    Testing the Dao

--- app/src/main/java/com/sorykhan/libroread/MainActivity.kt
+++ app/src/main/java/com/sorykhan/libroread/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.sorykhan.libroread
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -43,10 +43,9 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-//        intent = Intent(applicationContext, ReadActivity::class.java)
-//        startActivity(intent)
     }
+
+    fun updateFavorite(view: View) {}
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
